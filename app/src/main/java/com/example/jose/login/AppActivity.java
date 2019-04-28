5e629dca93052c9d9585f47f5034acfdc3b82c0a
Rafel37 <rafelperez37@hotmail.es>
2019-04-28 21:00:02 +0200

    prueba he cambiado AppActivity

--- app/src/main/java/com/example/jose/login/AppActivity.java
+++ app/src/main/java/com/example/jose/login/AppActivity.java
@@ -10,4 +10,5 @@ public class AppActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
     }
+    //sdawdasdasdasdasdazsdaw
 }
